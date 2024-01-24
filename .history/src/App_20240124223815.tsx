02e97e8fcf6ba82e44ee8f400250ad5a36ba3083
omri.alon <omri.alon@myheritage.com>
2024-01-24 22:40:33 +0200

    adding chakra UI

--- .history/src/App_20240124223815.tsx
+++ .history/src/App_20240124223815.tsx
@@ -1,6 +1,6 @@
 import { Button, ButtonGroup } from "@chakra-ui/react";
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  <Button colorScheme="blue">Button</Button>;
 }
 
 export default App;
