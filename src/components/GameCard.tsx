c06e26e4590e371c63becc7ae236686ae4c434ad
omri.alon <omri.alon@myheritage.com>
2024-01-25 11:51:16 +0200

    building game cards

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,17 @@
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+interface GameCardProps {
+  game: Game;
+}
+const GameCard = ({ game }: GameCardProps) => {
+  return (
+    <Card>
+      <CardBody borderRadius={10} overflow="hidden">
+        <Image src={game.background_image} />
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
