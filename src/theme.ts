acf0551d05a76ab7c67fda810af35224d902d39d
omri.alon <omri.alon@myheritage.com>
2024-01-26 17:54:40 +0200

    customize the theme to darker grays

--- src/theme.ts
+++ src/theme.ts
@@ -5,5 +5,18 @@ const config: ThemeConfig = {
 
 }
 
-const theme = extendTheme({config}); 
+const theme = extendTheme({config,
+colors: {
+    gray: {
+        50: '#f9f9f9',
+        100: '#ededed',
+        200: '#d3d3d3',
+        300: '#b3b3b3',
+        400: '#a0a0a0',
+        500: '#898989',
+        600: '#6c6c6c',
+        800: '#202020',
+        900: '#111'
+    }
+}}); 
 export default theme ;
