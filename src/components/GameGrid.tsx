e6b9923c20d1838732b34ea68e40e868ca0d2c62
omri.alon <omri.alon@myheritage.com>
2024-01-29 22:58:49 +0200

    implement infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,38 +1,59 @@
-import { Box, SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
+import { Box, Button, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGeners";
-import { GameQuery } from "../App";
-import { Platform } from "../hooks/usePlatform";
+import GameCardSkeleton from "./GameCardSkeleton";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-interface GameGridProps {
+interface Props {
   gameQuery: GameQuery;
 }
 
-const GameGrid = ({ gameQuery }: GameGridProps) => {
-  const { data, error, isLoading } = useGames(gameQuery);
+const GameGrid = ({ gameQuery }: Props) => {
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+
   if (error) return <Text>{error.message}</Text>;
+  const fetchedGamesCount =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-      padding={10}
-      spacing={6}
+    <InfiniteScroll
+      dataLength={fetchedGamesCount}
+      hasMore={!!hasNextPage}
+      next={() => fetchNextPage()}
+      loader={<Spinner />}
     >
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <GameCardContainer key={skeleton}>
-            <GameCardSkeleton />{" "}
-          </GameCardContainer>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={6}
+        padding="10px"
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))}
+          </React.Fragment>
         ))}
-      {data?.results.map((game) => (
-        <GameCardContainer key={game.id}>
-          <GameCard game={game} />
-        </GameCardContainer>
-      ))}
-    </SimpleGrid>
+      </SimpleGrid>
+    </InfiniteScroll>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,29 +1,38 @@
-import { useQuery } from "@tanstack/react-query";
-import { GameQuery } from "../App";
-import APIClient, { FetchResponse } from "../services/api-client";
-import { Platform } from "./usePlatform";
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { GameQuery } from '../App';
+import APIClient, {
+  FetchResponse,
+} from '../services/api-client';
+import { Platform } from './usePlatforms';
 
+const apiClient = new APIClient<Game>('/games');
 
-  export interface Game {
-    id: number;
-    name: string;
-    background_image: string,
-    parent_platforms: {platform: Platform}[],
-    metacritic: number,
-    rating_top:number
-  }
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
+}
 
+const useGames = (gameQuery: GameQuery) =>
+  useInfiniteQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: ({ pageParam = 1 }) =>
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+          page: pageParam
+        },
+      }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+    staleTime: 24 * 60 * 60 * 1000 //24hr
+  });
 
-const apiClient = new APIClient<Game>("/games") ;
-
-const useGames = (gameQuery:GameQuery)  => 
-useQuery<FetchResponse<Game>, Error>({
-  queryKey: ['games',gameQuery],
-  queryFn: () => (apiClient.getAll({params: { genres: gameQuery.genre?.id,
-    parent_platforms: gameQuery.platform?.id,
-    ordering: gameQuery.sortOrder,
-    search: gameQuery.searchText
-    }}))
-})
-
-export default useGames; 
+export default useGames;
