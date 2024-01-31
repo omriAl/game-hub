9275114334618266fafa84c3b6e1118b3de9df50
omri.alon <omri.alon@myheritage.com>
2024-01-31 21:09:51 +0200

    handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import GameDetailPage from "./pages/GameDetailPage";
 import HomePage from "./pages/homePage";
+
 import Layout from "./pages/Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetailPage /> },
